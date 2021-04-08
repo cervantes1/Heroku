cc10a56df4639f14e3a032a4c4d6aa3f3cc7e5ae
cervantes1 <rgcervan@ucsd.edu>
2021-04-07 18:22:51 -0700

    indent

--- app.py
+++ app.py
@@ -1,40 +1,4 @@
-# from flask import Flask
-# from flask_restful import Resource, Api, reqparse
-# import pandas as pd
-# import json
-
-# app = Flask(__name__)
-# api = Api(app)
-
-# @app.route("/")
-# def hello():
-#     return "<h1>Property Testing</h1><br><h3>/properties</h3><br><h3>/properties/{record}</h3>"
-
-# @app.route('/properties/<record>', methods=['GET'])
-# def get_records(record):
-#     with open('Data/data.json') as f:
-#         data = json.load(f)
-
-#     if record in data:
-#         return {record : data[record]}, 200
-
-# class properties(Resource):
-#     def get(self):
-#         with open('Data/data.json') as f:
-#             data = json.load(f)
-
-#         return {'Properties': data}, 200
-
-
-# api.add_resource(properties, '/properties')
-
-# if __name__ == "__main__": 
-#     app.run()  
-
-#     print("Testing")
- 
 from flask import Flask 
-# from flask_restful import Resource, Api, reqparse
 import json 
 
 app = Flask(__name__)
@@ -51,14 +15,13 @@ def get_records(record):
     if record in data:
         return {record : data[record]}, 200
 
-
-
 @app.route('/properties')
 def properties():
     with open('Data/data.json') as f:
         data = json.load(f)
 
-    return {'Properties': data}, 200  
+    s1 = json.dumps(data, indent=4, sort_keys=True)
+    return {'Properties': s1}, 200  
 
 if __name__ == '__main__':
     app.run()
