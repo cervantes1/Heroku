f064476592d025335275b31f1abc69f824b5e806
cervantes1 <rgcervan@ucsd.edu>
2021-04-07 18:12:36 -0700

    Trying

--- app.py
+++ app.py
@@ -1,35 +1,45 @@
-from flask import Flask
-from flask_restful import Resource, Api, reqparse
-import pandas as pd
-import json
+# from flask import Flask
+# from flask_restful import Resource, Api, reqparse
+# import pandas as pd
+# import json
+
+# app = Flask(__name__)
+# api = Api(app)
+
+# @app.route("/")
+# def hello():
+#     return "<h1>Property Testing</h1><br><h3>/properties</h3><br><h3>/properties/{record}</h3>"
+
+# @app.route('/properties/<record>', methods=['GET'])
+# def get_records(record):
+#     with open('Data/data.json') as f:
+#         data = json.load(f)
+
+#     if record in data:
+#         return {record : data[record]}, 200
+
+# class properties(Resource):
+#     def get(self):
+#         with open('Data/data.json') as f:
+#             data = json.load(f)
+
+#         return {'Properties': data}, 200
+
+
+# api.add_resource(properties, '/properties')
+
+# if __name__ == "__main__": 
+#     app.run()  
+
+#     print("Testing")
+ 
+from flask import Flask 
 
 app = Flask(__name__)
-api = Api(app)
 
-@app.route("/")
-def hello():
-    return "<h1>Property Testing</h1><br><h3>/properties</h3><br><h3>/properties/{record}</h3>"
+@app.route('/')
+def home():
+    return "Hi"
 
-@app.route('/properties/<record>', methods=['GET'])
-def get_records(record):
-    with open('Data/data.json') as f:
-        data = json.load(f)
-
-    if record in data:
-        return {record : data[record]}, 200
-
-class properties(Resource):
-    def get(self):
-        with open('Data/data.json') as f:
-            data = json.load(f)
-
-        return {'Properties': data}, 200
-
-
-api.add_resource(properties, '/properties')
-
-if __name__ == "__main__": 
-    app.run()  
-
-    print("Testing")
- 
+if __name__ == '__main__':
+    app.run()
