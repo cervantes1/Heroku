6f2f97f3cfd4ba730b4c59e5c47e86d4590241ae
cervantes1 <rgcervan@ucsd.edu>
2021-04-08 13:39:24 -0700

    jsonify

--- app.py
+++ app.py
@@ -13,16 +13,14 @@ def get_records(record):
         data = json.load(f)
 
     if record in data:
-        return {record : data[record]}, 200
+        return jsonify({record : data[record]}), 200
 
 @app.route('/properties')
 def properties():
     with open('Data/data.json') as f:
         data = json.load(f)
 
-    return json.dumps(data, indent=4, sort_keys=True), 200
-
-    # return {'Properties': data}, 200  
+    return jsonify({'Properties': data}), 200  
 
 if __name__ == '__main__':
     app.run()
